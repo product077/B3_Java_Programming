fcfaadb90c128f6c4ada7e9787bd68062b93ba4c
product077 <products0707@gmail.com>
2024-05-18 15:08:26 -0400

    Wrapper class

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -106,7 +106,29 @@ public class ArrayUtil {
     }
 
 
-    //TODO: create a reusable method that take 2 int array and return one array combined
+    /**
+     * This method returns new int array with combined element from two int arrays
+     * @param arr1 is an array where we will add an element to the beginning
+     * @param arr2 is an array where we will add an element to the end
+     * @return new array with combined elements
+     */
+    public static int [] addElemInArr (int [] arr1, int [] arr2) { // [0, 1, 1, 2, 3] - [4, 5]
+
+        int [] newArr = Arrays.copyOf(arr1, arr1.length+arr2.length); // [0, 1, 1, 2, 3, _, _]
+
+        for (int i = arr1.length; i < newArr.length; i++) {
+            newArr[i] = arr2[i - arr1.length]; // newArr[5] = arr2[5 - 5]; |  newArr[6] = arr2[6 - 5];
+        }
+
+        return newArr;
+    }
+
+
+    //TODO: create a reusable method that take 2 String array and return one array combined
+
+
+
+
 
 
     /**
@@ -144,6 +166,7 @@ public class ArrayUtil {
 
         return -1;
     }
+
 
     /**
      * This method sum up all the elements in int array
